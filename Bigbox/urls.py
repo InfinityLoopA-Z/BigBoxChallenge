a631a1929736f8315b22b1cad45cd8163ec09d12
felixhc <felixhumbertocarabajal5@gmail.com>
2021-10-19 17:48:28 -0300

    Adding changes in settings and preparing to future deploy

--- Bigbox/urls.py
+++ Bigbox/urls.py
@@ -1,6 +1,7 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(settings.ADMIN_URL, admin.site.urls),
 ]
