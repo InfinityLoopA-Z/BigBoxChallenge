957d5d003cf067e1272719e5f5002e315c6872c0
felixhc <felixhumbertocarabajal5@gmail.com>
2021-10-22 20:26:53 -0300

    Trying to solve refused connection to database

--- Bigbox/settings/production.py
+++ Bigbox/settings/production.py
@@ -5,7 +5,7 @@ from .base import env
 
 # Base
 
-SECRET_KEY = env('DJANGO_SECRET_KEY')
+SECRET_KEY = env.str('SECRET_KEY')
 ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['*'])
 
 # Port
