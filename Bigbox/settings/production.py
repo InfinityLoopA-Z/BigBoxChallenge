971bdb3aca952e9a8b433c06c88180c484293feb
felixhc <felixhumbertocarabajal5@gmail.com>
2021-10-22 19:49:15 -0300

    Solving an error in database connection

--- Bigbox/settings/production.py
+++ Bigbox/settings/production.py
@@ -15,7 +15,7 @@ PORT = env.int("PORT", 8000)
 
 # Databases
 
-DATABASES['default'] = env.db('HEROKU_POSTGRESQL_MAUVE_URL')  # NOQA
+DATABASES['default'] = env.db('HEROKU_POSTGRESQL_RED_URL')  # NOQA
 
 # Security
 
